ed14dc55b0ba2849e0fbd93f3a0acb4f52360787
Kinnar Kansara <kinnarrk@gmail.com>
2019-12-13 20:40:18 -0800

    Added flight recommendation

---
+++ finalproject/src/main/java/com/kinnar/bigdataproject/recommendation_sys/SecondarySortComparator.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.kinnar.bigdataproject.recommendation_sys;
+
+
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableComparator;
+
+public class SecondarySortComparator extends WritableComparator {
+    
+       protected SecondarySortComparator() {
+		super(CompositeKey.class, true);
+	}
+
+	@SuppressWarnings("rawtypes")
+	@Override
+	public int compare(WritableComparable a, WritableComparable b) {
+            
+         
+
+		CompositeKey ck1 = (CompositeKey) a;
+		CompositeKey ck2 = (CompositeKey) b;
+
+		int result = ck1.getSrcDest().compareTo(ck2.getSrcDest());
+
+		if (result == 0) {
+			String c1 = ck1.getCarrierInfo();
+			Double rms1 = Double.parseDouble(c1.split("\t")[1]);
+
+			String c2 = ck2.getCarrierInfo();
+			Double rms2 = Double.parseDouble(c2.split("\t")[1]);
+			result =  rms1.compareTo(rms2);
+			
+		}
+
+		return result;
+	}
+
+	
+	
+}
---
+++ finalproject/src/main/java/com/kinnar/bigdataproject/rms_carrier/RMSMain.java
@@ -0,0 +1,61 @@
+package com.kinnar.bigdataproject.rms_carrier;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+import java.io.IOException;
+import java.net.URI;
+
+public class RMSMain {
+
+    public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException{
+
+
+        Configuration conf = new Configuration();
+        
+        FileSystem hdfs = FileSystem.get(URI.create("hdfs://localhost:9000"),conf);
+		
+		Path output = new Path(args[1]);
+		// delete existing directory
+		if (hdfs.exists(output)) {
+		    hdfs.delete(output, true);
+		}
+		
+        // Create a new Job
+        Job job = Job.getInstance(conf,"wordcount");
+        job.setJarByClass(RMSMain.class);
+
+        // Specify various job-specific parameters
+        job.setJobName("myjob");
+
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, output);
+
+
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(RMSCountTuple.class);
+
+
+        job.setMapperClass(RMSMapper.class);
+        job.setCombinerClass(RMSCombiner.class);
+        job.setReducerClass(RMSReducer.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(RMSCountTuple.class);
+
+        // Submit the job, then poll for progress until the job is complete
+        System.exit(job.waitForCompletion(true)?0:1);
+
+    }
+}
